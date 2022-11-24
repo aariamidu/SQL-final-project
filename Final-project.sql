6ebe4fb33ba0ee179fcece6d2c4114dd56a432d3
aariamidu <aarifah.amidu@gmail.com>
2022-11-24 18:54:15 +0000

    Update Final-project.sql
    Values added to table

--- Final-project.sql
+++ Final-project.sql
@@ -3,4 +3,6 @@ USE Members_info;
 CREATE TABLE Members_info(ID varchar(20), First_name varchar(20),Last_name varchar(20),Email varchar(50),Total_points integer);
 SELECT * FROM Members_info;
 INSERT INTO Members_info(ID,First_name,Last_name,Email,Total_points)
-VALUES(
+VALUES(1,"Sam","Jones","samjones@mail.com",20),(2,"Jenny","Hill","jennyhill@mail.com",15),(3, "Sarah","Smith","sarahsmith@mail.com",12),(4,"Max","Green","maxgreen@mail.com",5),(6,"Jessica","Jackson","jessicajackson@mail.com",10),(7,"Abbie","Mitchell","abbiemitchell@mail.com",1),(8,"Nia","Bonadie","niabonadie@mail.com",18),(9,"Scott","Cooper","scottcooper@mail.com",25),(10,"Reece","Walsh","reecewalsh@mail.com",17);
+SHOW TABLES;
+
